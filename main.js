72d250c14e1aa3eb9945e012b371588325aa783a
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2024-01-02 23:09:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ for (i = 0; i < 69; i++) {
       images[i].style.position = "absolute"
       images[i].src = "frames/frame_0000"+i+".png"
       images[i].style.visibility = "hidden"
+      images[i].style.height = "50%"
       document.getElementById("images").appendChild(images[i])
     } else {
       images[i] = new Image()
       images[i].style.position = "absolute"
       images[i].src = "frames/frame_000"+i+".png"
       images[i].style.visibility = "hidden"
+      images[i].style.height = "500px"
+      images[i].style.height = "50%"
       document.getElementById("images").appendChild(images[i])
     }
 }
@@ -85,7 +88,7 @@ function addFrame() {
   var temp = btns.length + 1
   newBtn.onclick = function() {moveTo(temp)}
   btns.push(newBtn)
-  document.body.appendChild(btns[btns.length - 1])
+  document.getElementById("frames").appendChild(btns[btns.length - 1])
   frames.push([])
   console.log(btns)
   moveTo(btns.length)
@@ -137,17 +140,74 @@ function playButton() {
       moveTo(counter)
     }, 83.33)
     playBtn.innerText = "Stop"
+    disableButtons()
   } else {
     playing = false
     clearInterval(player)
     playBtn.innerText = "Play"
+    enableButtons()
   }
+}
+
+function disableButtons() {
+  document.getElementById("selectAllBtn").disabled = true
+  document.getElementById("selectAllBtn").style.color = "grey"
+  document.getElementById("selectAllBtn").style.borderColor = "grey"
+  document.getElementById("deselectAllBtn").disabled = true
+  document.getElementById("deselectAllBtn").style.color = "grey"
+  document.getElementById("deselectAllBtn").style.borderColor = "grey"
+  document.getElementById("addFrameBtn").disabled = true
+  document.getElementById("addFrameBtn").style.color = "grey"
+  document.getElementById("addFrameBtn").style.borderColor = "grey"
+  document.getElementById("removeFrameBtn").disabled = true
+  document.getElementById("removeFrameBtn").style.color = "grey"
+  document.getElementById("removeFrameBtn").style.borderColor = "grey"
+  document.getElementById("copyFrameBtn").disabled = true
+  document.getElementById("copyFrameBtn").style.color = "grey"
+  document.getElementById("copyFrameBtn").style.borderColor = "grey"
+  document.getElementById("pasteFrameBtn").disabled = true
+  document.getElementById("pasteFrameBtn").style.color = "grey"
+  document.getElementById("pasteFrameBtn").style.borderColor = "grey"
+  document.getElementById("generateCode").disabled = true
+  document.getElementById("generateCode").style.color = "grey"
+  document.getElementById("generateCode").style.borderColor = "grey"
+}
+
+function enableButtons() {
+  document.getElementById("selectAllBtn").disabled = false
+  document.getElementById("selectAllBtn").style.color = "white"
+  document.getElementById("selectAllBtn").style.borderColor = "white"
+  document.getElementById("deselectAllBtn").disabled = false
+  document.getElementById("deselectAllBtn").style.color = "white"
+  document.getElementById("deselectAllBtn").style.borderColor = "white"
+  document.getElementById("addFrameBtn").disabled = false
+  document.getElementById("addFrameBtn").style.color = "white"
+  document.getElementById("addFrameBtn").style.borderColor = "white"
+  document.getElementById("removeFrameBtn").disabled = false
+  document.getElementById("removeFrameBtn").style.color = "white"
+  document.getElementById("removeFrameBtn").style.borderColor = "white"
+  document.getElementById("copyFrameBtn").disabled = false
+  document.getElementById("copyFrameBtn").style.color = "white"
+  document.getElementById("copyFrameBtn").style.borderColor = "white"
+  document.getElementById("pasteFrameBtn").disabled = false
+  document.getElementById("pasteFrameBtn").style.color = "white"
+  document.getElementById("pasteFrameBtn").style.borderColor = "white"
+  document.getElementById("generateCode").disabled = false
+  document.getElementById("generateCode").style.color = "white"
+  document.getElementById("generateCode").style.borderColor = "white"
 }
 
 addFrame()
 moveTo(1)
+selectAll()
+deselectAll()
 
+var generateCodeButton = document.getElementById("generateCode")
 
+function copyCode() {
+  navigator.clipboard.writeText(document.getElementById("arduinoCode").innerText)
+  console.log("text copied")
+}
 
 
 
